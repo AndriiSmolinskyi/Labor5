b574e4060937b46fb86f0f60e6dd62653f83fadf
AndriiSmolinskyi <awful.braid@gmail.com>
2023-01-02 23:07:53 +0200

    Labor5
    Labor5

---
+++ Labor5/Labor5.cpp
@@ -0,0 +1,31 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+void Swap(int* a, int* b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
+
+
+int main()
+{
+	int var1 = 15;
+	int var2 = 20;
+
+	cout << "var1\t" << var1 << endl;
+	cout << "var2\t" << var2 << endl;
+
+	cout << "Swap" << endl;
+	Swap(&var1, &var2);
+
+	cout << "var1\t" << var1 << endl;
+	cout << "var2\t" << var2 << endl;
+
+
+
+}
